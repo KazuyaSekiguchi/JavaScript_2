c987347d7f8f9e1e811814e9e723979be563162b
KazuyaSekiguchi <kzy.sekiguchi0908@gmail.com>
2023-03-16 14:04:25 +0000

    JavaScript課題2

--- main3.js
+++ main3.js
@@ -1,18 +1,19 @@
 function clickbutton(target) {
   let result = document.getElementById("result")
-  let target_value = target.innerHTML;
-  
+  let target_value = target.innerHTML
   if (target_value == "AC") {
     result.innerHTML = "0"
-  } else if (target_value == "=") {
+  } else if (target_value == "=" ) {
     result.innerHTML = eval(result.innerHTML)
+  } else if (target_value == "+" || target_value == "-" || target_value == "*" || target_value == "/" || target_value == "." ) {
+    result.innerHTML = eval(result.innerHTML) +  target.innerHTML.slice(-1);
   } else {
     if (result.innerHTML == "0") {
-      result.innerHTML = target_value
+      if (target_value != "0" && target_value != "00" ) {
+        result.innerHTML = target_value
+      }
     } else {
       result.innerHTML += target_value
     }
-   
   }
-
 }
